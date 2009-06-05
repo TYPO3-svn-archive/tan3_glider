42be9151e586df25915103d0beb9945c2d98d279
caplod <caplod@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-05 09:46:30 +0000

    Version 1.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tan3_glider/tags/1.0.1@21052 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "tan3_glider"
+#
+# Auto generated 04-06-2009 18:59
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Glider',
+	'description' => 'Inserts a glider with content elements. A demo can be found here: http://jqueryfordesigners.com/demo/coda-slider.html',
+	'category' => 'plugin',
+	'author' => 'Stefan Mielke',
+	'author_email' => 'stefan@tan3.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => 0,
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '1.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:26:{s:9:"ChangeLog";s:4:"4a3a";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"b8fb";s:17:"ext_localconf.php";s:4:"6d9a";s:14:"ext_tables.php";s:4:"9637";s:15:"flexform_ds.xml";s:4:"96ff";s:13:"locallang.xml";s:4:"3e82";s:16:"locallang_db.xml";s:4:"f0aa";s:14:"doc/manual.sxw";s:4:"fdaa";s:19:"doc/wizard_form.dat";s:4:"a57f";s:20:"doc/wizard_form.html";s:4:"5d2c";s:14:"pi1/ce_wiz.gif";s:4:"b555";s:31:"pi1/class.tx_tan3glider_pi1.php";s:4:"059f";s:39:"pi1/class.tx_tan3glider_pi1_wizicon.php";s:4:"53c9";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"2d02";s:18:"res/arrow_left.png";s:4:"e151";s:19:"res/arrow_right.png";s:4:"fd0b";s:14:"res/glider.css";s:4:"e188";s:13:"res/glider.js";s:4:"d097";s:23:"res/jquery-1.3.2.min.js";s:4:"bb38";s:35:"res/jquery.localscroll-1.2.7-min.js";s:4:"8996";s:32:"res/jquery.scrollTo-1.4.2-min.js";s:4:"4aa3";s:36:"res/jquery.serialScroll-1.2.1-min.js";s:4:"c82d";s:27:"static/glider/constants.txt";s:4:"d41d";s:23:"static/glider/setup.txt";s:4:"aef0";}',
+	'suggests' => array(
+	),
+);
+
+?>
